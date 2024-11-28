56314016f82f75bdc640e34bdc9eda7afa5000ed
Kamalika Arumugam <akamalika1225@gmail.com>
2024-11-28 15:03:11 +0530

    Add files via upload

---
+++ practical 3.py
@@ -0,0 +1,17 @@
+length=int(input("Enter length:"))
+breath=int(input("Enter breath:"))
+area_of_rectangle=length*breath
+print("The Area of Rectangle:",area_of_rectangle)
+
+base=int(input("Enter base"))
+height=int(input("Enter height"))
+area_of_triangle=1/2*base*height
+
+print("The Area of Triangle:",area_of_triangle)
+a=int(input("Enter area:"))
+area_of_square=a*a
+print("The Area of Square:" ,area_of_square)
+
+radius=int(input("Enter radius:"))
+area_of_circle=22/7*radius*radius
+print("The Area of  Circle:",area_of_circle)
