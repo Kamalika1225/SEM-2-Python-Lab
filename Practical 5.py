6a0e689f4fd4392eca730cab16f448098e70c750
Kamalika Arumugam <akamalika1225@gmail.com>
2024-12-10 19:31:45 +0530

    Add files via upload

---
+++ Practical 5.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+num=int(input('Enter the number:'))
+print("The factorial is ",factorial(num))
+
+
+
+
+
+
+
